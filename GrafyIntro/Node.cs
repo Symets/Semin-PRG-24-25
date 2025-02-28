f4eaf7d0d64f599cf692f8174859b4efddb696ed
Symets <spunt.dzeletovic@gmail.com>
2025-02-28 09:50:57 +0100

    day 159 of not propperly using github

---
+++ GrafyIntro/Node.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrafyIntro
+{
+    internal class Node
+    {
+        public int index;
+        public List<Node> neighbours;
+
+        public Node(int index) 
+        {
+            this.index = index;
+            neighbours = new List<Node>();
+        }
+
+        public void AddNeighbours(List<Node> neighboursToAdd)
+        {
+            foreach(Node neighbour in neighboursToAdd)
+            {
+                if (neighbours.Contains(neighbour))
+                {
+                    Console.WriteLine("Neighbour " +  neighbour.index + " is already in list.");
+                }
+                else
+                {
+                    neighbours.Add(neighbour);
+                }
+            }
+            //neighbours.AddRange(neighboursToAdd);  - idk bro yaps rlly fast
+
+        }
+
+        public void PrintNeighbours()
+        {
+            Console.Write("Node " + index + " has neighbours ");
+            foreach(Node neighbour in neighbours)
+            {
+                Console.Write(neighbour.index + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public Node TraverseNode(int indexToTraverse)
+        {
+            foreach(Node neighbour in neighbours)
+            {
+                if(neighbour.index == indexToTraverse)
+                {
+                    return neighbour;
+                }
+            }
+            return this;
+        }
+    }
+}
---
+++ GrafyIntro/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("GrafyIntro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GrafyIntro")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("d6ad7cb2-1b17-4501-aa53-187ad6c81e84")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
